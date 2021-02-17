ca7cc6502ea37065dd63cab9481ec8f269e2ecf1
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-17 16:05:08 +0100

    fix

--- seedJob.groovy
+++ seedJob.groovy
@@ -1,13 +1,18 @@
-import groovy.json.JsonSlurper
+import jenkins.*
+import jenkins.model.*
+import hudson.*
+import hudson.model.*
+import java.util.Arrays
+import java.util.Lists
+import java.util.stream.Collectors
+import java.util.stream.Stream
+import groovy.json.*
  
  
-// filename = 
-// println(filename)
-def a = getClass().protectionDomain.codeSource.location.path
-println(a)
+println (env.WORKSPACE)
 
  
-def jsonSlurper = new JsonSlurper()
-data = jsonSlurper.parse(new File("jobs.json"))
-println(data)
-println ("WSK")
+// def jsonSlurper = new JsonSlurper()
+// data = jsonSlurper.parse(new File("jobs.json"))
+// println(data)
+// println ("WSK")
