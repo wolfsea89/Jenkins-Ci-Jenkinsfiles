e222971e422abc7d1ca556df3ad25944b4be39fc
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-17 16:05:42 +0100

    fix

--- seedJob.groovy
+++ seedJob.groovy
@@ -3,7 +3,7 @@ import jenkins.model.*
 import hudson.*
 import hudson.model.*
 import java.util.Arrays
-import java.util.Lists
+import java.util.List
 import java.util.stream.Collectors
 import java.util.stream.Stream
 import groovy.json.*
