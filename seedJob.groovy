57727b4341801f8fc2f577c7db56f93fde13e623
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-17 18:57:55 +0100

    fix

--- seedJob.groovy
+++ seedJob.groovy
@@ -8,11 +8,21 @@ for (job in jobs){
   pipelineJob(job.name) {
     parameters{
       stringParam("branch", job.defaultBranch, 'Branch name')
+      stringParam("repositoryUrl", job.repositoryUrl, 'Repository URL (git/https)')
+      stringParam("manualVersion", "", 'Set manual version (X.Y.Z). Worked with branch release, hotfix, master without version')
+      environmentVariables {
+        env('JENKINSFILE_SCRIPTS_DIR', '.jenkins')
+        env('GIT_CREDS_ID', 'github')
+        env('APP_CONFIGURATION_JSON_PATH', 'configuration/jenkins.json')
+        env('BASEIMAGE_SERVICES_ADMIN_CREDS_ID', 'baseImage_services_AminPassword')
+        env('DOCKER_REPOSITORY_CREDS_ID', 'docker_hub')
+        env('DOCKER_REPOSITORY_URL', 'https://index.docker.io/v1/')
+        env('DOCKER_REPOSITORY_SNAPSHOT_NAME', 'wolfsea89/${projectName}_snapshot')
+        env('DOCKER_REPOSITORY_RELEASE_NAME', 'wolfsea89/${projectName}')
+      }
     }
   }
 }
-
-
  
 // def jsonSlurper = new JsonSlurper()
 // data = jsonSlurper.parse(new File("jobs.json"))
