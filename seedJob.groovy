bb7b475f6c2ca7762d215ac107dffb2187d035d5
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-17 15:14:55 +0100

    fix

--- seedJob.groovy
+++ seedJob.groovy
@@ -1,1 +1,9 @@
+import groovy.json.JsonSlurper
+ 
+ 
+filename = new File("jobs.json")
+ 
+def jsonSlurper = new JsonSlurper()
+data = jsonSlurper.parse(new File(filename))
+println(data)
 println ("WSK")
