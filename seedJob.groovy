c3c989dd8667371444064a7099b80d53b5e5f6fc
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-17 15:30:48 +0100

    fix

--- seedJob.groovy
+++ seedJob.groovy
@@ -3,6 +3,7 @@ import groovy.json.JsonSlurper
  
 // filename = 
 // println(filename)
+println(env.WORKSPACE)
  
 def jsonSlurper = new JsonSlurper()
 data = jsonSlurper.parse(new File("jobs.json"))
